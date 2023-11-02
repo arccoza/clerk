cd7cf32d326fff65dbd15c9a6261637e38154408
Adrien <adrien@arccoza.com>
2023-11-02 16:27:33 +0800

    feat: add src and entry files with basic impl

---
+++ src/main.js
@@ -0,0 +1,56 @@
+import GObject from 'gi://GObject';
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk?version=4.0';
+import Adw from 'gi://Adw?version=1';
+import { ClerkWindow } from './window.js';
+
+
+pkg.initGettext();
+pkg.initFormat();
+
+export const ClerkApplication = GObject.registerClass(
+  class ClerkApplication extends Adw.Application {
+    constructor() {
+      super({application_id: 'com.arccoza.clerk', flags: Gio.ApplicationFlags.DEFAULT_FLAGS});
+
+      const quit_action = new Gio.SimpleAction({name: 'quit'});
+      quit_action.connect('activate', action => {
+        this.quit();
+      });
+      this.add_action(quit_action);
+      this.set_accels_for_action('app.quit', ['<primary>q']);
+
+      const show_about_action = new Gio.SimpleAction({name: 'about'});
+      show_about_action.connect('activate', action => {
+        let aboutParams = {
+          transient_for: this.active_window,
+          application_name: 'clerk',
+          application_icon: 'com.arccoza.clerk',
+          developer_name: 'Adrien',
+          version: '0.1.0',
+          developers: [
+            'Adrien'
+            ],
+          copyright: '© 2023 Adrien'
+        };
+        const aboutWindow = new Adw.AboutWindow(aboutParams);
+        aboutWindow.present();
+      });
+      this.add_action(show_about_action);
+    }
+
+    vfunc_activate() {
+      let {active_window} = this;
+
+      if (!active_window)
+        active_window = new ClerkWindow(this);
+
+      active_window.present();
+    }
+  }
+  );
+
+export function main(argv) {
+  const application = new ClerkApplication();
+  return application.runAsync(argv);
+}
---
+++ src/window.js
@@ -0,0 +1,67 @@
+import GObject from 'gi://GObject';
+import Gtk from 'gi://Gtk';
+import Adw from 'gi://Adw';
+
+
+export const ClerkWindow = GObject.registerClass({
+  GTypeName: 'ClerkWindow',
+  Template: 'resource:///com/arccoza/clerk/window.ui',
+  InternalChildren: ["filesAdd", "mediaSearchInput", "filesUpdate", "filePicker", "files"],
+}, class ClerkWindow extends Adw.ApplicationWindow {
+  constructor(application) {
+    super({ application });
+  }
+
+  async onFilesAdd(button) {
+    console.log("onFilesAdd")
+    // const res = await this._filePicker.open_multiple(this, null, null)
+    // const res = await this._filePicker.open_multiple_finish()
+    // console.log("--->", res)
+    this._filePicker.show()
+  }
+
+  onMediaSearchChanged(button) {
+    console.log("onMediaSearchChanged", button)
+  }
+
+  onFilesUpdate(button) {
+    console.log("onFilesUpdate", button)
+  }
+
+  onFilesAdded(filePicker, responseId) {
+    console.log("onFilesAdded", responseId)
+    if (responseId !== Gtk.ResponseType.ACCEPT) {
+     return
+    }
+
+    const files = filePicker.get_files()
+    this.addFiles(files)
+  }
+
+  setupFileItem(listView, listItem) {
+    const row = new Adw.ActionRow
+    listItem.child = row
+  }
+
+  bindFileItem(listView, listItem) {
+    const file = listItem.item
+    const row = listItem.child
+    row.icon_name = "checkbox"
+    row.title = file.get_basename()
+    row.subtitle = file.get_path()
+  }
+
+  addFiles(files) {
+    for (let i = 0, file; file = files.get_item(i), !!file && i < 1000; i++) {
+      console.log("onFilesAdded", file, file.get_basename())
+      this._files.append(file)
+    }
+  }
+
+  remFiles(fileIndices) {
+    for (const i of fileIndices) {
+      this._files
+    }
+  }
+});
+

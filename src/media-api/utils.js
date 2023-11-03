682fc41a5401740626c071dec21f3e2e9afde9d4
Adrien <adrien@arccoza.com>
2023-11-04 03:58:35 +0800

    fix: filter catching false

--- src/media-api/utils.js
+++ src/media-api/utils.js
@@ -1,3 +1,8 @@
 export function makeSearchParams(obj) {
-  return encodeURI(Object.entries(obj).filter(([k, v]) => !!v).map(([k, v]) => `${k}=${v}`).join("&"))
+  return encodeURI(
+    Object.entries(obj)
+      .filter(([k, v]) => v != null && v !== "")
+      .map(([k, v]) => `${k}=${v}`)
+      .join("&")
+  )
 }

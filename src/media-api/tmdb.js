4e8577fc500764905c05a7cfdc467b19534d4806
Adrien <adrien@arccoza.com>
2023-11-05 23:25:37 +0800

    feat: add details api endpoint to TMDB

--- src/media-api/tmdb.js
+++ src/media-api/tmdb.js
@@ -3,6 +3,10 @@ import { makeSearchParams } from "./utils"
 
 export class TMDB {
   _baseUrl = "https://api.themoviedb.org/3"
+  _headers = {
+    accept: "application/json",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTliM2M1M2JjNzY5N2M0NWQzZGQ1ZmYzYzE3ZDFjMCIsInN1YiI6IjU4MmQyYTMxOTI1MTQxMDk1ZDAwMjY2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a1p8_PtthYMxddvjQpCedWB93BBWKXKhuLWjAwHmzUk"
+  }
 
   constructor(language="en-US") {
     this._language = language
@@ -22,19 +26,28 @@ export class TMDB {
 
     const url = `${this._baseUrl}/search/${type}?${params}`
 
-    console.log("------>>>", url)
-
     const options = {
       method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTliM2M1M2JjNzY5N2M0NWQzZGQ1ZmYzYzE3ZDFjMCIsInN1YiI6IjU4MmQyYTMxOTI1MTQxMDk1ZDAwMjY2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a1p8_PtthYMxddvjQpCedWB93BBWKXKhuLWjAwHmzUk"
-      }
+      headers: this._headers,
     }
 
     return fetch(url, options)
       .then((res) => res.json())
-      // .then(json => console.log(json))
-      // .catch(err => console.error("error:" + err))
+  }
+
+  async details(type, id) {
+    if (!id) {
+      throw "Missing id"
+    }
+
+    const url = `${this._baseUrl}/${type}/${id}?language=${this._language}`
+
+    const options = {
+      method: "GET",
+      headers: this._headers,
+    }
+
+    return fetch(url, options)
+      .then((res) => res.json())
   }
 }

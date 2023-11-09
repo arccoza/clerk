cfcbfc0b9b194a5156aac93a848a159d3eb3abac
Adrien <adrien@arccoza.com>
2023-11-09 23:10:05 +0800

    chore: record soup experiments

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import Adw from "gi://Adw?version=1"
 import { MediaInfo } from "./gobjects"
 import { MediaPicker } from "./MediaPicker"
 import { ClerkWindow } from "./window.js"
+import Soup from 'gi://Soup'
 
 
 pkg.initGettext()
@@ -54,6 +55,34 @@ export const ClerkApplication = GObject.registerClass(
   )
 
 export function main(argv) {
+  // const url = 'https://api.themoviedb.org/3/search/tv?query=scooby%20doo%20where%20are%20you&include_adult=false&language=en-US&page=1';
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     accept: 'application/json',
+  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTliM2M1M2JjNzY5N2M0NWQzZGQ1ZmYzYzE3ZDFjMCIsInN1YiI6IjU4MmQyYTMxOTI1MTQxMDk1ZDAwMjY2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a1p8_PtthYMxddvjQpCedWB93BBWKXKhuLWjAwHmzUk'
+  //   }
+  // };
+
+  // fetch(url, options)
+  //   .then(res => (console.log(res), res))
+  //   .then(res => res.json())
+  //   .then(json => console.log(json))
+  //   .catch(err => console.error('error:' + err));
+
+
+  // const session = new Soup.Session();
+  // const msg = Soup.Message.new('GET', url);
+  // const request_headers = msg.get_request_headers();
+  // request_headers.append('accept', 'application/json');
+  // request_headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTliM2M1M2JjNzY5N2M0NWQzZGQ1ZmYzYzE3ZDFjMCIsInN1YiI6IjU4MmQyYTMxOTI1MTQxMDk1ZDAwMjY2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a1p8_PtthYMxddvjQpCedWB93BBWKXKhuLWjAwHmzUk');
+  // session.queue_message(msg, (_, {statusCode, responseBody}) => {
+  //   log(`statusCode: ${statusCode}`);
+  //   log(`body: ${responseBody.data}`);
+  // });
+  // const gbytes = session.send_and_read(msg, null)
+  // console.log(new TextDecoder().decode(gbytes.toArray()))
+
   const application = new ClerkApplication()
   return application.runAsync(argv)
 }
